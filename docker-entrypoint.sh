6685bce32dc778450ead578f7ae4a4f882be34d9
Paolo Mainardi <paolo@twinbit.it>
2015-08-31 18:08:02 +0200

    update entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -64,19 +64,23 @@ if [ "$1" = 'mysqld' ]; then
 			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
 			mysql+=( "$MYSQL_DATABASE" )
 		fi
-    if [ "$MYSQL_DATABASE2" ]; then
-      echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE2\` ;" | "${mysql[@]}"
-      mysql+=( "$MYSQL_DATABASE2" )
-    fi
+
+		if [ "$MYSQL_DATABASE2" ]; then
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE2\` ;" | "${mysql[@]}"
+			mysql+=( "$MYSQL_DATABASE2" )
+		fi
 
 		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
 			echo "CREATE USER '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"' ;" | "${mysql[@]}"
+
 			if [ "$MYSQL_DATABASE" ]; then
 				echo "GRANT ALL ON \`"$MYSQL_DATABASE"\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
 			fi
-      if [ "$MYSQL_DATABASE2" ]; then
-        echo "GRANT ALL ON \`"$MYSQL_DATABASE2"\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
-      fi
+
+			if [ "$MYSQL_DATABASE2" ]; then
+				echo "GRANT ALL ON \`"$MYSQL_DATABASE2"\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
+			fi
+
 			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
 		fi
 
